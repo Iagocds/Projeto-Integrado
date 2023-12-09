58ea6c9841b4116b143d6e3a6b6f8b8512a644dd
Iagocds <65873598+Iagocds@users.noreply.github.com>
2023-12-09 15:41:13 -0300

    Correção para o BUG #1
    Correção para o BUG de abertura de arquivo

--- amostra.c
+++ amostra.c
@@ -7,28 +7,21 @@ int main(void)
 {
     FILE *arquivo;
 
-    //Abertura do arquivo
-    arquivo = fopen("dados","w");
+    arquivo = fopen("dados","w"); //Abertura do arquivo
     if(arquivo == NULL) //Tratamento de erro do arquivo
     {
-        printf("Erro ao gerar ou abrir o arquivo, aperte qualquer tecla para continuar");
-        system("pause");
+        printf("Erro ao gerar ou abrir o arquivo\n");
         return 1;
     }
 
-    //Gravar o numero de variaveis gerado na primeira linha do documento
+    fprintf(arquivo, "%d\n", MAX); //Gravar o numero de variaveis gerado na primeira linha do documento
 
-    fprintf(arquivo, "%d\n", MAX);
-
-
-    //Geração dos numeros aleatorios
-    int i;
-    for (i = 0; i < MAX; i++)
+    int i;  
+    for (i = 0; i < MAX; i++) //Geração dos numeros aleatorios
     {
         fprintf(arquivo, "%d\n", rand() % 100); //Gerando um numero de 0 a 100 e gravando no arquivo.
     }
 
-    //Fechar arquivo
-    fclose(arquivo);
+    fclose(arquivo); //Fechar arquivo
     return 0;
 }
